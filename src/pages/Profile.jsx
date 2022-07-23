0ca8d33befe2e3a06f09fb817f0c25fef137007a
Simon Mac Aree <35202475+Simonbiker@users.noreply.github.com>
2022-07-23 12:42:12 +0100

    Adding the ability for the user to see the listings they have added through the profile page.

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,17 +1,21 @@
 import React from 'react'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import { getAuth, updateProfile } from 'firebase/auth'
-import { updateDoc, doc } from 'firebase/firestore'
+import { updateDoc, doc, collection, getDocs, query, where, orderBy, deleteDoc } from 'firebase/firestore'
 import {db} from '../firebase.config'
 import { useNavigate } from 'react-router-dom'
 import { toast } from 'react-toastify'
+import ListingItem from '../components/ListingItem'
 import { Link } from 'react-router-dom'
 import arrowRight from '../assets/svg/keyboardArrowRightIcon.svg'
 import homeIcon from '../assets/svg/homeIcon.svg'
+import { async } from '@firebase/util'
 
 
 function Profile() {
   const auth = getAuth()
+  const [loading, setLoading] = useState(true)
+  const [listings, setListings] = useState(null)
   const [changeDetails, setChangeDetails] = useState(false)
   const [formData, setFormData] = useState({
     name: auth.currentUser.displayName,
@@ -21,6 +25,26 @@ function Profile() {
   const {name, email} = formData
 
   const navigate = useNavigate()
+  useEffect(() => {
+    const fetchUserListings = async () => {
+      const listingsRef = collection(db, 'listings')
+      const q = query(listingsRef, where('userRef', '==', auth.currentUser.uid), orderBy('timestamp', 'desc'))
+      const querySnap = await getDocs(q)
+
+      let listings = []
+      querySnap.forEach((doc) => {
+        return listings.push({
+          id: doc.id, 
+          data: doc.data()
+        })
+      })
+      setListings(listings)
+      setLoading(false)
+    }
+
+    fetchUserListings()
+    
+  }, [auth.currentUser.uid])
 
   const onLogout = () => {
     auth.signOut()
@@ -50,6 +74,16 @@ function Profile() {
       ...prevState,
       [e.target.id]: e.target.value
     }))
+  }
+
+  const onDelete = async (listingId) => {
+    if (window.confirm('Are you sure you want to delete?')) {
+      await deleteDoc(doc(db, 'listings', listingId))
+      const updatedListings = listings.filter(
+        (listing) => listing.id !== listingId)
+      setListings(updatedListings)
+      toast.success('Sucessfully deleted listing')
+    }
   }
   
   return <div className='profile'>
@@ -85,6 +119,20 @@ function Profile() {
         <p>Sell or rent your home</p>
         <img src={arrowRight} alt="arrow right" />
       </Link>
+
+      {!loading && listings?.length > 0 && (
+        <>
+          <p className='listingText'>Your lisitngs</p>
+          <ul className="listingsList">
+            {listings.map((listing) => (
+              <ListingItem key={listing.id}
+                listing={listing.data}
+                id={listing.id}
+                onDelete={ () => onDelete(listing.id)} />
+            ))}
+          </ul>
+        </>
+      )}
     </main>
   </div>
 }
